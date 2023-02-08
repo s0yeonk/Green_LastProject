38739fe81b41650ffe443febbf35036ec3dd3f87
김소연 <tcb05019@naver.com>
2023-02-08 17:27:30 +0900

    file변경

--- footer/footer.js
+++ footer/footer.js
@@ -1,5 +1,5 @@
 const footer = document.querySelector('#footer');
 
-fetch('/LastProject/footer/footer.html')
+fetch('../footer/footer.html')
 .then(res => res.text())
 .then(data => footer.innerHTML = data)
